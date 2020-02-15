e97d2bae448600334c7c437163b457d50f2ecc5e
Nikki Sz <nikkisz@Nikkis-MBP.home>
2020-02-15 19:22:48 +0000

    Initial commit

---
+++ src/main/java/com/apiGateway/zuul/ApiGatewayWithZuulApplication.java
@@ -0,0 +1,13 @@
+package com.apiGateway.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayWithZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayWithZuulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiGateway/zuul/ApiGatewayWithZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiGateway.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayWithZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
