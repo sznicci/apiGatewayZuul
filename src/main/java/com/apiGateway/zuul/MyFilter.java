9eb44409a40150034d8355ccf3b55bde037a20b6
Nikki Sz <nikkisz@Nikkis-MBP.home>
2020-02-15 19:54:54 +0000

    Added Filter class

---
+++ src/main/java/com/apiGateway/zuul/MyFilter.java
@@ -0,0 +1,35 @@
+package com.apiGateway.zuul;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext requestContext = RequestContext.getCurrentContext();
+		requestContext.addZuulRequestHeader("my-auth-token", "s3cret");
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return FilterConstants.PRE_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+	}
+
+}
